ac81430cb11bc8cd1c564ca465c4aeefde9bdcf6
Afshin Arani <afshin@arani.dev>
2025-12-28 14:16:13 +0000

    wip

--- NArk.Transport.GrpcClient.Tests/TransportTests.cs
+++ NArk.Transport.GrpcClient.Tests/TransportTests.cs
@@ -3,6 +3,8 @@
 public class TransportTests
 {
     [Test]
+    // Network is never reliable...
+    [Retry(5)]
     public void CanConnectToMainnetArk()
     {
         var transport = new GrpcClientTransport("https://arkade.computer");
